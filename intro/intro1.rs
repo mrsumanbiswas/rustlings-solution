f444744cff7be2e35f74b34a006fe66464ee9293
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-01-12 01:32:21 +0530

    feat: create intro1.rs

---
+++ intro/intro1.rs
@@ -0,0 +1,21 @@
+// intro1.rs
+
+// I AM DONE
+
+fn main() {
+    println!("Hello and");
+    println!(r#"       welcome to...                      "#);
+    println!(r#"                 _   _ _                  "#);
+    println!(r#"  _ __ _   _ ___| |_| (_)_ __   __ _ ___  "#);
+    println!(r#" | '__| | | / __| __| | | '_ \ / _` / __| "#);
+    println!(r#" | |  | |_| \__ \ |_| | | | | | (_| \__ \ "#);
+    println!(r#" |_|   \__,_|___/\__|_|_|_| |_|\__, |___/ "#);
+    println!(r#"                               |___/      "#);
+    println!();
+    println!("This exercise compiles successfully. The remaining exercises contain a compiler");
+    println!("or logic error. The central concept behind Rustlings is to fix these errors and");
+    println!("solve the exercises. Good luck!");
+    println!();
+    println!("The source for this exercise is in `exercises/intro/intro1.rs`. Have a look!");
+    println!("Going forward, the source of the exercises will always be in the success/failure output.");
+}
