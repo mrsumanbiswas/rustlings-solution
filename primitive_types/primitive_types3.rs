2b15f555c6ecf2b4ca6a4641d342a46570082c99
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-01-12 02:22:45 +0530

    feat: create primitive_types3.rs

---
+++ primitive_types/primitive_types3.rs
@@ -0,0 +1,13 @@
+// primitive_types3.rs
+
+// I AM DONE
+
+fn main() {
+    let a = [0; 100];
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+    }
+}
