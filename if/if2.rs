1fe6c0d44bb541027a74bf0b88a7112fef7d0ffc
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-01-12 02:08:48 +0530

    feat: create if2.rs

---
+++ if/if2.rs
@@ -0,0 +1,34 @@
+// if2.rs
+
+// I AM DONE
+
+pub fn foo_if_fizz(fizzish: &str) -> &str {
+    if fizzish == "fizz" {
+        "foo"
+    } else if fizzish == "fuzz" {
+        "bar"
+    } else {
+        "baz"
+    }
+}
+
+// No test changes needed!
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn foo_for_fizz() {
+        assert_eq!(foo_if_fizz("fizz"), "foo")
+    }
+
+    #[test]
+    fn bar_for_fuzz() {
+        assert_eq!(foo_if_fizz("fuzz"), "bar")
+    }
+
+    #[test]
+    fn default_to_baz() {
+        assert_eq!(foo_if_fizz("literally anything"), "baz")
+    }
+}
