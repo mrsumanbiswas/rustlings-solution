bf64e33c8b81977e8218868347bc9267123fbb7f
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-01-12 02:32:16 +0530

    feat: create primitive_types6.rs

---
+++ primitive_types/primitive_types6.rs
@@ -0,0 +1,11 @@
+// primitive_types6.rs
+
+// I AM DONE
+
+#[test]
+fn indexing_tuple() {
+    let numbers = (1, 2, 3);
+    let second = numbers.1;
+
+    assert_eq!(2, second, "This is not the 2nd number in the tuple!")
+}
