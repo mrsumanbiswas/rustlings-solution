a444533aad35104ede5c84d4cc58d1a0b58e4aac
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-01-12 02:29:24 +0530

    feat: create primitive_types4.rs

---
+++ primitive_types/primitive_types4.rs
@@ -0,0 +1,12 @@
+// primitive_types4.rs
+
+// I AM DONE
+
+#[test]
+fn slice_out_of_array() {
+    let a = [1, 2, 3, 4, 5];
+
+    let nice_slice = &a[1..4];
+
+    assert_eq!([2, 3, 4], nice_slice)
+}
