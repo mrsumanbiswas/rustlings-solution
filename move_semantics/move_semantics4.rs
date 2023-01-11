02e974446fa3aff488b9acd070fd7fb45376909f
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-01-12 03:10:02 +0530

    update move_semantics4.rs

--- move_semantics/move_semantics4.rs
+++ move_semantics/move_semantics4.rs
@@ -1,11 +1,11 @@
 // move_semantics4.rs
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    // let vec0 = Vec::new();
 
-    let mut vec1 = vec![vec0..fill_vec()];
+    let mut vec1 = fill_vec();
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -16,7 +16,7 @@ fn main() {
 
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument
 fn fill_vec() -> Vec<i32> {
-    let mut vec = vec;
+    let mut vec = Vec::new();
 
     vec.push(22);
     vec.push(44);
