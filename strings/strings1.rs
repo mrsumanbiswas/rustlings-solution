0a6e6c432e61f74b570ad395dacb1d156574be9b
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-01-12 12:05:01 +0530

    feat: create strings1.rs

---
+++ strings/strings1.rs
@@ -0,0 +1,12 @@
+// strings1.rs
+
+// I AM DONE
+
+fn main() {
+    let answer = current_favorite_color();
+    println!("My current favorite color is {}", answer);
+}
+
+fn current_favorite_color() -> String {
+    "blue".to_string()
+}
