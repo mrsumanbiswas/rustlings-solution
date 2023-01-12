ff3a9e9ec345422663814af702314aa6c4347125
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-01-12 11:32:25 +0530

    feat: create enums1.rs

---
+++ enums/enums1.rs
@@ -0,0 +1,18 @@
+// enums1.rs
+
+// I AM DONE
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Echo,
+    Move,
+    ChangeColor,
+}
+
+fn main() {
+    println!("{:?}", Message::Quit);
+    println!("{:?}", Message::Echo);
+    println!("{:?}", Message::Move);
+    println!("{:?}", Message::ChangeColor);
+}
